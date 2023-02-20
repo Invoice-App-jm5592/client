37ced8153983683abbb2c561a521e37f2e6095f2
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-02-20 13:29:58 +0100

    Setup Vue Router

---
+++ src/router/index.ts
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import InvoiceListView from "../views/Invoice/ListView.vue";
+import InvoiceDetailsView from "../views/Invoice/DetailsView.vue";
+
+import NotFoundView from "../views/NotFoundView.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      redirect: "/invoices",
+    },
+    {
+      path: "/invoices",
+      name: "invoices",
+      component: InvoiceListView,
+    },
+    {
+      path: "/invoices/:id",
+      name: "invoice-details",
+      component: InvoiceDetailsView,
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "not-found",
+      component: NotFoundView,
+    },
+  ],
+});
+
+export default router;
