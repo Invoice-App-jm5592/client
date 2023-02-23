c89f299c9b219dc88f7dfc10a35a2fb2adc821fd
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-02-24 00:45:04 +0100

    Define "white" color

--- src/common/components/BaseTypography.vue
+++ src/common/components/BaseTypography.vue
@@ -1,14 +1,14 @@
 <template>
-  <p v-if="props.size === 'sm'" class="text-sm">
+  <p v-if="props.size === 'sm'" class="text-sm text-white">
     <slot />
   </p>
-  <p v-if="props.size === 'body'" class="text-body">
+  <p v-if="props.size === 'body'" class="text-body text-white">
     <slot />
   </p>
-  <h2 v-if="props.size === 'md'" class="text-md">
+  <h2 v-if="props.size === 'md'" class="text-md text-white">
     <slot />
   </h2>
-  <h1 v-if="props.size === 'lg'" class="text-lg">
+  <h1 v-if="props.size === 'lg'" class="text-lg text-white">
     <slot />
   </h1>
 </template>
