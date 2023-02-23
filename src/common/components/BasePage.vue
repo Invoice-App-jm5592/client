efab6bcd34869e72f914b33522963e0a81f5e7af
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-02-24 00:44:54 +0100

    Add BasePage Component

---
+++ src/common/components/BasePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex">
+    <TheSidebar />
+    <div class="w-full bg-light-black min-h-screen">
+      <div class="max-w-3xl mx-auto">
+        <div class="py-16 px-8">
+          <slot name="header" />
+        </div>
+        <div class="pb-16 px-8">
+          <slot name="content" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import TheSidebar from './TheSidebar.vue';
+</script>
