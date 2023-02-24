22807e1ee2f5cdcde3185f3b446b34fd4a5eff91
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-02-24 16:51:14 +0100

    Make BaseButton more dynamic

--- src/common/components/BaseButton.vue
+++ src/common/components/BaseButton.vue
@@ -1,13 +1,14 @@
 <template>
   <button 
-    @click="onClick"
+    @click="props.onClick"
     class="rounded-3xl bg-dark-violet hover:bg-light-violet transition-all flex items-center"
+    :class="classes"
   > 
-    <span class="my-2 ml-2 rounded-full bg-white w-8 h-8 flex items-center justify-center">
-      <PlusIcon />
+    <span v-if="$slots.icon" class="my-2 ml-2 rounded-full bg-white w-8 h-8 flex items-center justify-center">
+      <slot name="icon" />
     </span>
     <BaseTypography is-bold styles="m-4">
-      {{ props.label }}
+      <slot name="label" />
     </BaseTypography>
   </button>
 </template>
@@ -15,21 +16,16 @@
 <script lang="ts" setup>
 import { defineProps, PropType } from 'vue';
 import BaseTypography from './BaseTypography.vue';
-import PlusIcon from '../components/icons/PlusIcon.vue';
 
-// eslint-disable-next-line no-unused-vars
-type onClickHandler = (payload: MouseEvent) => void;
+type onClickHandler = () => void;
 
 const props = defineProps({
-  label: {
-    type: String,
-    require: true
-  },
   onClick: {
     type: Function as PropType<onClickHandler>,
     require: true
   },
-  icon: {
+  classes: {
+    type: String,
     default: ""
   }
 });
