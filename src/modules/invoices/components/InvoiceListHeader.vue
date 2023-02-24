c43b4b510deab83025973f1b5c692e514fdc94c9
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-02-24 16:51:25 +0100

    Add BaseButton to InvoiceListHeader

---
+++ src/modules/invoices/components/InvoiceListHeader.vue
@@ -0,0 +1,34 @@
+
+import BaseTypography from '@/common/components/BaseTypography.vue';
+
+<template>
+  <div class="flex justify-between items-center">
+    <div>
+      <BaseTypography size="lg" styles="mb-2">
+        Invoices
+      </BaseTypography>
+      <BaseTypography>
+        There are 3 total invoices
+      </BaseTypography>
+    </div>
+    <div class="flex items-center">
+      <BaseTypography is-bold>
+        Filter by status
+      </BaseTypography>
+      <BaseButton label="New Invoice">
+        <template #icon>
+          <PlusIcon />
+        </template>
+        <template #label>
+          New Invoice
+        </template>
+      </BaseButton>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import BaseButton from '@/common/components/BaseButton.vue';
+import BaseTypography from '@/common/components/BaseTypography.vue';
+import PlusIcon from '@/common/components/icons/PlusIcon.vue';
+</script>
