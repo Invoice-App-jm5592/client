0d2a2894d64baea32171ddb8920dec13ca895b95
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-02-26 16:45:32 +0100

    Add LeftDrawer

---
+++ src/common/components/TheLeftDrawer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-if="props.isVisible"
+    class="h-[100vh] w-[35vw] overflow-x-hidden overflow-y-auto bg-light-black absolute top-0 left-[100px] rounded-tr-2xl rounded-br-2xl z-[200] p-6"
+    >
+    <slot />
+  </div>
+  <div @click="clickAwayListener" v-if="props.isVisible"
+    class="z-[199] bg-black h-[100vh] w-[100vw] absolute top-0 opacity-80" />
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  isVisible: {
+    type: Boolean,
+    require: true,
+    default: false
+  }
+});
+
+const emits = defineEmits(['clickAway']);
+const clickAwayListener = () => {
+  emits('clickAway');
+}
+</script>
--- src/modules/invoices/components/InvoiceListHeader.vue
+++ src/modules/invoices/components/InvoiceListHeader.vue
@@ -1,6 +1,3 @@
-
-import BaseTypography from '@/common/components/BaseTypography.vue';
-
 <template>
   <div class="flex justify-between items-center">
     <div>
@@ -13,7 +10,7 @@ import BaseTypography from '@/common/components/BaseTypography.vue';
     </div>
     <div class="flex items-center">
       <InvoiceFilter />
-      <BaseButton label="New Invoice">
+      <BaseButton label="New Invoice" @click="toggleDrawer">
         <template #icon>
           <PlusIcon />
         </template>
@@ -23,22 +20,32 @@ import BaseTypography from '@/common/components/BaseTypography.vue';
       </BaseButton>
     </div>
   </div>
+  <TheLeftDrawer :is-visible="isDrawerOpen" @click-away="toggleDrawer">
+    <BaseTypography>This is the left drawer</BaseTypography>
+  </TheLeftDrawer>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { useStore } from 'vuex';
 import BaseButton from '@/common/components/BaseButton.vue';
 import BaseTypography from '@/common/components/BaseTypography.vue';
 import PlusIcon from '@/common/components/icons/PlusIcon.vue';
+import TheLeftDrawer from '@/common/components/TheLeftDrawer.vue';
 import InvoiceFilter from './InvoiceFilter.vue';
 
 const store = useStore();
+
+const isDrawerOpen = ref(false);
 
 const invoicesOverviewTitle = computed(() => {
   if (store.getters.isInvoicesListEmpty) {
     return 'No invoices';
   }
-  return `There are ${ store.getters.totalInvoices } total invoices`;
+  return `There are ${store.getters.totalInvoices} total invoices`;
 });
+
+const toggleDrawer = () => {
+  isDrawerOpen.value = !isDrawerOpen.value;
+}
 </script>
