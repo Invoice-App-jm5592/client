8ebbe8b42a591ab95e816a1ba451b2394e437d1c
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-03-11 22:59:29 +0100

    Create axios instance

---
+++ src/common/config/axios.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: process.env.VUE_APP_API_BASE_URL,
+  headers: {
+    Accept: "application/json",
+  },
+});
+
+export default instance;
