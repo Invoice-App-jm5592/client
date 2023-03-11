f9c3926720c04fbe2254bad778af2ff59b5e6da8
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-03-11 23:37:33 +0100

    Add Error and Loading Spinner to ListView

--- src/views/Invoice/ListView.vue
+++ src/views/Invoice/ListView.vue
@@ -4,7 +4,9 @@
       <InvoiceListHeader />
     </template>
     <template #content>
-      <InvoiceListEmpty v-if="store.getters.isInvoicesListEmpty" />
+      <BaseLoadingSpinner v-if="isLoading" />
+      <BaseError v-else-if="error" :message="error" />
+      <InvoiceListEmpty v-else-if="store.getters.isInvoicesListEmpty" />
       <InvoiceList v-else :list="store.getters.invoicesList" />
     </template>
   </BasePage>
@@ -12,21 +14,28 @@
 
 <script setup lang="ts">
 import { useStore } from 'vuex';
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import BasePage from '@/common/components/BasePage.vue';
 import InvoiceList from '@/modules/invoices/components/InvoiceList.vue';
 import InvoiceListHeader from '@/modules/invoices/components/InvoiceListHeader.vue';
 import InvoiceListEmpty from '@/modules/invoices/components/InvoiceListEmpty.vue';
 import axiosInstance from '@/common/config/axios';
+import BaseError from '@/common/components/BaseError.vue';
+import BaseLoadingSpinner from '@/common/components/BaseLoadingSpinner.vue';
 
 const store = useStore();
+const isLoading = ref(true);
+const error = ref('');
 
 onMounted(async () => {
   try {
+    isLoading.value = true;
     const response = await axiosInstance.get('/api/invoices');
     store.commit('addInvoicesToList', response.data.invoices);
-  } catch (error) {
-    console.error(error);
+  } catch (_err) {
+    error.value = 'Something went wrong fetching your invoices. Please try again.'
+  } finally {
+    isLoading.value = true;
   }
 });
 </script>
