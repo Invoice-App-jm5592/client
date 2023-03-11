bd5f045b18b6cd5baebd13f42f9ab6db33e2810a
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-03-11 23:36:55 +0100

    Create BaseError Component

---
+++ src/common/components/BaseError.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <BaseTypography size="md" styles="font-bold text-red">{{ props.message }}</BaseTypography>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import BaseTypography from '@/common/components/BaseTypography.vue';
+
+const props = defineProps({
+  message: {
+    type: String,
+    require: true
+  }
+});
+</script>
